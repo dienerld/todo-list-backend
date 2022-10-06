ed71df549e66622413b7ea34db35ee8657e747a8
Diener Dornelas <dienezim@hotmail.com>
2022-10-06 20:25:49 -0300

    update env

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 
 import { app } from './app';
 
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
