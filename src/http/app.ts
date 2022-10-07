34f7ebb022c3259ffda3d7d9ace73825f944a8fd
Diener Dornelas <dienezim@hotmail.com>
2022-10-07 20:39:57 -0300

    add cors

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { cors } from './middleware/cors';
 import { usersRouter, tasksRouter } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 app.use('/users', usersRouter);
 app.use('/tasks', tasksRouter);
