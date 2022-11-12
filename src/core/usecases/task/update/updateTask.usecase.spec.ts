29c8b2143b1ac7e02b97c14e0ad523d6a2329b9d
Diener Dornelas <dienezim@hotmail.com>
2022-11-12 11:31:13 -0300

    should return 400 when update task with invalid id

--- src/core/usecases/task/update/updateTask.usecase.spec.ts
+++ src/core/usecases/task/update/updateTask.usecase.spec.ts
@@ -28,5 +28,18 @@ describe('[UseCase] Update Task', () => {
 
     expect(statusCode).toBe(400);
     expect(body).toHaveProperty('error', 'NotFoundError');
+    expect(body).toHaveProperty('message', 'Task not found');
+  });
+  it('should return 404 if user not found', async () => {
+    const { sut } = makeSut();
+    const [task] = UsersMock[0].tasks;
+
+    const { statusCode, body } = await sut.execute(task.user_id + '1', task.id, {
+      title: 'any_title'
+    });
+
+    expect(statusCode).toBe(400);
+    expect(body).toHaveProperty('error', 'NotFoundError');
+    expect(body).toHaveProperty('message', 'User not found');
   });
 });
