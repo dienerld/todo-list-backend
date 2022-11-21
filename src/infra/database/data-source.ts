fadf95883bff5c04c8e723873f47da7b21c114ad
Diener Dornelas <dienezim@hotmail.com>
2022-11-21 13:47:39 -0300

    update env

--- src/infra/database/data-source.ts
+++ src/infra/database/data-source.ts
@@ -7,7 +7,7 @@ import { taskSchema } from './schemas/task.schema';
 const appDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  logging: true,
+  logging: process.env.NODE_ENV !== 'production',
   // synchronize: true,
   entities: [userSchema, taskSchema],
   migrations: [__dirname + '/migrations/*.{ts,js}']
