d11c3c6f9e7a67214766250d81e72a45e7149300
Diener Dornelas <dienezim@hotmail.com>
2022-11-21 20:12:56 -0300

    hotfix: update user

--- src/core/usecases/user/update/updateUser.usecase.ts
+++ src/core/usecases/user/update/updateUser.usecase.ts
@@ -24,7 +24,8 @@ class UpdateUserUseCase {
         user.email = userDto.email;
       }
 
-      if (userDto.password !== undefined || userDto.password_confirm !== undefined) {
+      if (userDto.password?.trim() &&
+        (userDto.password !== undefined || userDto.password_confirm !== undefined)) {
         if (userDto.password !== userDto.password_confirm) {
           throw new InvalidParamError('Password does not match');
         }
