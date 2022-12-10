21962155bfc6a24dc41b27d5008ad5e89245fd84
Diener Dornelas <dienezim@hotmail.com>
2022-12-09 21:22:53 -0300

    add cache

--- src/core/usecases/user/delete/deleteUser.usecase.ts
+++ src/core/usecases/user/delete/deleteUser.usecase.ts
@@ -1,9 +1,13 @@
 import { IUserRepository } from '@models/user/userRepository.interface';
+import { IRepositoryCache } from '@presentation/cache/repositoryCache.interface';
 import { CustomError, NotFoundError } from '@presentation/errors';
 import { HttpResponse, IHttpResponse } from '@presentation/helpers';
 
 class DeleteUserUsecase {
-  constructor (private readonly userRepository: IUserRepository) {}
+  constructor (
+    private readonly userRepository: IUserRepository,
+    private readonly cacheRepository: IRepositoryCache
+  ) {}
 
   async execute (userId: string): Promise<IHttpResponse> {
     try {
@@ -11,8 +15,11 @@ class DeleteUserUsecase {
       if (!user) {
         throw new NotFoundError('User');
       }
-      await this.userRepository.delete(userId);
-
+      Promise.all([
+        await this.userRepository.delete(userId),
+        await this.cacheRepository.delete(userId),
+        await this.cacheRepository.delete(`user-with-tasks${userId}`)
+      ]);
       return HttpResponse.noContent();
     } catch (error) {
       if (error instanceof CustomError) {
