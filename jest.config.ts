fa68e8c48df36d7f7f956257860c054cf0ab2c73
Diener Dornelas <dienezim@hotmail.com>
2022-12-15 19:34:26 -0300

    set  new jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,15 @@
-import { compilerOptions } from './tsconfig.json';
 
 export default {
-  transform: {
-    '^.+\\.ts?$': '@swc/jest'
-  },
+  transform: { '^.+\\.ts?$': '@swc/jest' },
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['**/src/**/*.ts'],
   testMatch: ['**/*.{test,spec}.ts'],
-  // moduleNameMapper: {
-  // '@models/(.*)': '<rootDir>/src/core/models/$1',
-  // '@presentation/(.*)': '<rootDir>/src/core/presentation/$1',
-  // '@infra/(.*)': '<rootDir>/src/infra/$1',
-  // '@database/(.*)': '<rootDir>/src/database/$1',
-  // '@configs/(.*)': '<rootDir>/src/configs/$1'
-  // }
-  moduleNameMapper: compilerOptions.paths
+  moduleNameMapper: {
+    '@models/(.*)': '<rootDir>/src/core/models/$1',
+    '@presentation/(.*)': '<rootDir>/src/core/presentation/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1',
+    '@database/(.*)': '<rootDir>/src/database/$1',
+    '@configs/(.*)': '<rootDir>/src/configs/$1'
+  }
 };
