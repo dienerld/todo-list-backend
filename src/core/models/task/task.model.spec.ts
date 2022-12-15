1b299ce0dfc6f2455a8c490eeac6dd65648207cc
Diener Dornelas <dienezim@hotmail.com>
2022-12-15 20:36:06 -0300

    tests to update task

--- src/core/models/task/task.model.spec.ts
+++ src/core/models/task/task.model.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../presentation/errors/missingParamsError';
+import { MissingParamError } from '@presentation/errors';
 import { Task } from './task.model';
 
 describe('[Model] Task', () => {
@@ -23,7 +23,49 @@ describe('[Model] Task', () => {
 
   it('should throw if no date is provided', () => {
     expect(
-      () => Task.create('Test', null, '12:00', 'any_id')
+      () => Task.create('Test', null as unknown as Date, '12:00', 'any_id')
     ).toThrow(new MissingParamError('Date'));
+  });
+
+  it('should update date', async () => {
+    const date = new Date(Date.now() - 1000 * 60 * 60);
+    const task = Task.create('Test', date, '12:00', 'any_id');
+
+    task.update({ date: new Date() });
+
+    expect(task).toHaveProperty('date');
+    expect(task.date).not.toEqual(date);
+  });
+
+  it('should update hour', async () => {
+    const task = Task.create('Test', new Date(), '12:00', 'any_id');
+
+    task.update({ hour: '13:00' });
+
+    expect(task).toHaveProperty('hour', '13:00');
+  });
+
+  it('should update title', async () => {
+    const task = Task.create('Test', new Date(), '12:00', 'any_id');
+
+    task.update({ title: 'Test 2' });
+
+    expect(task).toHaveProperty('title', 'Test 2');
+  });
+
+  it('should update done', async () => {
+    const task = Task.create('Test', new Date(), '12:00', 'any_id');
+
+    task.update({ done: true });
+
+    expect(task).toHaveProperty('done', true);
+  });
+
+  it('should update hidden', async () => {
+    const task = Task.create('Test', new Date(), '12:00', 'any_id');
+
+    task.update({ hidden: true });
+
+    expect(task).toHaveProperty('hidden', true);
   });
 });
