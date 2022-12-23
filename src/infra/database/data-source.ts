9cee0abe90c286cb30bffa0a281570cc37afbd39
Diener Dornelas <dienezim@hotmail.com>
2022-12-23 11:02:25 -0300

    set differents profiles

--- src/infra/database/data-source.ts
+++ src/infra/database/data-source.ts
@@ -1,15 +1,48 @@
 /* eslint-disable n/no-path-concat */
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import 'dotenv/config';
 import { userSchema } from './schemas/user.schema';
 import { taskSchema } from './schemas/task.schema';
 
-const appDataSource = new DataSource({
+const { NODE_ENV } = process.env;
+
+const cfgDev: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  logging: process.env.NODE_ENV === 'dev',
+  logging: true,
   entities: [userSchema, taskSchema],
-  migrations: [__dirname + '/migrations/*.{ts,js}']
-});
+  migrations: [__dirname + '/migrations/*.ts']
+};
+
+const cfgTest: DataSourceOptions = {
+  type: 'sqlite',
+  database: ':memory:',
+  logging: false,
+  entities: [userSchema, taskSchema],
+  migrations: [__dirname + '/migrations/*.ts']
+};
+
+const cfgProd: DataSourceOptions = {
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  logging: false,
+  entities: [userSchema, taskSchema],
+  migrations: [__dirname + '/migrations/*.js']
+};
+
+function setCfg () {
+  switch (NODE_ENV) {
+    case 'test':
+      return cfgTest;
+    case 'dev':
+      return cfgDev;
+    case 'prod':
+      return cfgProd;
+    default:
+      return cfgDev;
+  }
+}
+
+const appDataSource = new DataSource(setCfg());
 
 export { appDataSource };
