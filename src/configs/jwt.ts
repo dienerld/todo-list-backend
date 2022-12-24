2ba237e805d72aa0290c0cc4cfa0af7c7c8a9913
Diener Dornelas <dienezim@hotmail.com>
2022-12-24 17:57:08 -0300

    get env to file envs

--- src/configs/jwt.ts
+++ src/configs/jwt.ts
@@ -1,5 +1,7 @@
+import { envs } from './env';
+
 const jwtConfig = {
-  secret: process.env.SECRET_JWT || '65b1fb37-df6a-42c3-bffb-8d7521d74413',
+  secret: envs.jwtSecret || '65b1fb37-df6a-42c3-bffb-8d7521d74413',
   expiresIn: '1d'
 };
 
