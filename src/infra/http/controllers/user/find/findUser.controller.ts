22bb5ed4417bb8a5a7aae843d5202d6d7090be8e
Diener Dornelas <dienezim@hotmail.com>
2022-12-26 11:49:19 -0300

    remove unncessary code

--- src/infra/http/controllers/user/find/findUser.controller.ts
+++ src/infra/http/controllers/user/find/findUser.controller.ts
@@ -6,19 +6,11 @@ class FindUserController {
   constructor (private readonly findUserUseCase: FindUserUseCase) {}
 
   async handle (request: CustomRequest, response: Response) {
-    try {
-      const userId = request.user?.id;
-      if (!userId) {
-        return response.status(400).json({ error: 'User not found' });
-      }
+    const userId = request.user!.id;
 
-      const { body, statusCode } = await this.findUserUseCase.execute(userId);
+    const { body, statusCode } = await this.findUserUseCase.execute(userId);
 
-      return response.status(statusCode).json(body);
-    } catch (err: any) {
-      return response.status(400).json(
-        { error: err.message || 'Unexpected error.' });
-    }
+    return response.status(statusCode).json(body);
   }
 }
 
